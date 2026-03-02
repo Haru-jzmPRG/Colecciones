03c92bc842d7446d4853be15e9a2f25bd1ea45f7
Jazmin <programajazminc20@gmail.com>
2026-03-02 19:17:29 +0100

    implementado equals, hashcode y compareTo

--- Ejercicio_10/src/App.java
+++ Ejercicio_10/src/App.java
@@ -3,28 +3,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HashMap<String, String> traductor = new HashMap<String, String>();
+        HashMap<String, Palabra> traductor = new HashMap<String, Palabra>();
 
-        traductor.put("amanecer", "sunrise");
-        traductor.put("desafío", "challenge");
-        traductor.put("esperanza", "hope");
-        traductor.put("libertad", "freedom");
-        traductor.put("fortaleza", "strength");
-        traductor.put("sabiduría", "wisdom");
-        traductor.put("alegría", "joy");
-        traductor.put("tristeza", "sadness");
-        traductor.put("sueño", "dream");
-        traductor.put("recuerdo", "memory");
-        traductor.put("camino", "path / way");
-        traductor.put("cambio", "change");
-        traductor.put("miedo", "fear");
-        traductor.put("amistad", "friendship");
-        traductor.put("amor", "love");
-        traductor.put("éxito", "success");
-        traductor.put("fracaso", "failure");
-        traductor.put("trabajo", "work");
-        traductor.put("tiempo", "time");
-        traductor.put("vida", "life");
+        traductor.put("amanecer", new Palabra("sunrise", "sustantivo"));
+        traductor.put("desafío", new Palabra("challenge", "sustantivo"));
+        traductor.put("esperanza", new Palabra("hope", "sustantivo"));
+        traductor.put("libertad", new Palabra("freedom", "sustantivo"));
+        traductor.put("fortaleza", new Palabra("strength", "sustantivo"));
+        traductor.put("sabiduría", new Palabra("wisdom", "sustantivo"));
+        traductor.put("alegría", new Palabra("joy", "sustantivo"));
+        traductor.put("tristeza", new Palabra("sadness", "sustantivo"));
+        traductor.put("sueño", new Palabra("dream", "sustantivo"));
+        traductor.put("recuerdo", new Palabra("memory", "sustantivo"));
+        traductor.put("camino", new Palabra("path", "sustantivo"));
+        traductor.put("cambio", new Palabra("change", "sustantivo"));
+        traductor.put("miedo", new Palabra("fear", "sustantivo"));
+        traductor.put("amistad", new Palabra("friendship", "sustantivo"));
+        traductor.put("amor", new Palabra("love", "sustantivo"));
+        traductor.put("éxito", new Palabra("success", "sustantivo"));
+        traductor.put("fracaso", new Palabra("failure", "sustantivo"));
+        traductor.put("trabajo", new Palabra("work", "sustantivo"));
+        traductor.put("tiempo", new Palabra("time", "sustantivo"));
+        traductor.put("vida", new Palabra("life", "sustantivo"));
 
         System.out.println("Ingrese una palabra: ");
         Scanner sc = new Scanner(System.in);
@@ -32,9 +32,10 @@ public class App {
         sc.close();
 
         if (traductor.containsKey(palabra)) {
-            System.out.println(palabra + " = " + traductor.get(palabra));
+            Palabra p = traductor.get(palabra);
+            System.out.println(palabra + " = " + p.getTraduccion() + " (" + p.getCategoria() + ")");
         } else {
-            System.out.println("NULL = La palabra no se encuentra en el traductor.");
+            System.out.println("NULL = La palabra no se encuentra en el traductor."); 
         }
     }
 }
