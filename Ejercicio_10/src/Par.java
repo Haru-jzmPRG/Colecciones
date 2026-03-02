b8a97a0ad99e2b2fcabe43a4edf1dc512e7f592e
Jazmin <programajazminc20@gmail.com>
2026-03-02 19:26:13 +0100

    practica: escribir una clase generica

---
+++ Ejercicio_10/src/Par.java
@@ -0,0 +1,18 @@
+public class Par<A, B> {
+    private A first;
+    private B second;
+
+    public Par(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+}
